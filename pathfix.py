bb99cda8b951444338288f8816b1180da8f0b830
Lance Paje <lpaje@uoguelph.ca>
2019-09-23 18:53:09 -0400

    realy round 1 over

---
+++ pathfix.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main(args):
+    print("Hello world!")
+    input = args[0]
+    
+    #>>> modify 0A input here
+    with open(input, 'w') as f:
+        for line in f:
+            
+
+
+
+    if :
+      return input #>>>file 
+    else:
+        return 0  #>>>stdout
+    
+
+if __name__ == "__main__":
+    main(sys.argv)
